4f171b69a46124a6ea4177493ef658905643f981
Tamme <wintam@live.nl>
2021-10-05 17:11:09 +0200

    File input/submit toegevoegd, page lay-out klaar.

--- public/berichten.php
+++ public/berichten.php
@@ -1,22 +1,30 @@
+<!-- Programmeur: Tamme Tuncil
+Datum: 05-10-2021
+Opdracht: Project, berichten pagina. -->
 <!DOCTYPE html>
 <html>
-    <head>
-        <!-- Template include voor header en navbar -->
-        <?php include "../templates/header.php"; ?>
-        <!-- Imported stylecode voor het font Roboto -->
-        <style> @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');</style>
+<head>
+	<!-- Template include voor de header -->
+	<?php include "../templates/header.php"; ?>
+    <!-- Imported stylecode voor het font Roboto -->
+	<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+	</style>
+	<title>berichten</title>
 </head>
 <body>
-    <div id="container-berichten">
-        <div class="item-1">
-            <h1>Upload een afbeelding</h1>    
-        </div>
-        <div class="item-2">
-            <h1>Bericht</h1>
-        </div>
-        <div class="item-3">
-            <h1>Plaats afbeelding</h1>
-        </div>
-    </div>
+	<div id="container-berichten">
+		<div class="item-1">
+			<label class="uploadImage" for="fotoFile">Upload een afbeelding</label> 
+            <input id="fotoFile" type="file">
+		</div>
+		<div class="item-2">
+			<h1>Bericht</h1>
+		</div>
+		<div class="item-3">
+			<label class="submitImageButton" for="submitImage">Plaats afbeelding</label> 
+            <input id="submitImage" type="submit">
+		</div>
+	</div>
 </body>
 </html>

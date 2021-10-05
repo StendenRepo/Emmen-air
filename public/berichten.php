6fafe99317a3f2cacb6c532a7a8e247c23a66ea4
Tamme <wintam@live.nl>
2021-10-05 16:10:04 +0200

    css styling / page layout toegepast.

--- public/berichten.php
+++ public/berichten.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <?php include "../templates/header.php"; ?>
+<html>
+    <head>
+        <!-- Template include voor header en navbar -->
+        <?php include "../templates/header.php"; ?>
+        <!-- Imported stylecode voor het font Roboto -->
+        <style> @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');</style>
 </head>
 <body>
-    
+    <div id="container-berichten">
+        <div class="item-1">
+            <h1>Upload een afbeelding</h1>    
+        </div>
+        <div class="item-2">
+            <h1>Bericht</h1>
+        </div>
+        <div class="item-3">
+            <h1>Plaats afbeelding</h1>
+        </div>
+    </div>
 </body>
 </html>

5fad7f358cd6c9dc01a847b82f267004c598f68c
Tamme <wintam@live.nl>
2021-10-06 10:14:27 +0200

    form action toegevoegd + button animation

--- public/berichten.php
+++ public/berichten.php
@@ -1,31 +1,33 @@
-<!-- Programmeur: Tamme Tuncil
+<!-- 
+Programmeur: Tamme Tuncil
 Datum: 05-10-2021
-Opdracht: Project, berichten pagina. -->
+Opdracht: Project, berichten pagina.
+-->
 <!DOCTYPE html>
 <html>
 <head>
 	<!-- Template include voor de header -->
 	<?php include "../templates/header.php"; ?>
-    <!-- Imported stylecode voor het font Roboto -->
+	<!-- Imported stylecode voor het font Roboto -->
 	<style>
-	@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+	   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 	</style>
 	<title>Berichten</title>
 </head>
 <body>
 	<div id="container-berichten">
 		<div class="item-1">
-			<label class="uploadImage" for="fotoFile">Upload een afbeelding</label> 
-            <input id="fotoFile" type="file">
+			<label class="uploadImage" for="fotoFile">Upload een afbeelding</label> <input id="fotoFile" type="file">
 		</div>
 		<div class="item-2">
-            <label for="textareaBerichten"></label>
-            <textarea id="textareaBerichten" name=textarea placeholder="Vul hier uw bericht in..."></textarea>
+			<form action="berichten.php">
+				<textarea id="textareaBerichten" name="textarea" placeholder="Vul hier uw bericht in..."></textarea> 
+				<input id="submitImage" type="submit">
+			</form>
 		</div>
 		<div class="item-3">
-			<label class="submitImageButton" for="submitImage">Plaats afbeelding</label> 
-            <input id="submitImage" type="submit">
+			<label class="submitImageButton" for="submitImage">Plaats afbeelding</label>
 		</div>
 	</div>
 </body>
-</html> 
+</html>

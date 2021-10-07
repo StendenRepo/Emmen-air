0deda51c28ce433ffc19af25ec368aafd5804496
sandio20 <dion.stomp@student.nhlstenden.com>
2021-10-07 09:50:00 +0200

    extra voorbeeld artikel toegevoegd

--- public/infopage.php
+++ public/infopage.php
@@ -80,6 +80,57 @@
                         </div>
                     </div>
                 </div>
+                <div class=artikel-box>
+                    <div class="artikel-veld">
+                        <div class=afbeelding>
+                            <img src=assets/vliegtuig.png alt="vliegtuig"/>
+                        </div>
+                        <div class=titel>
+                            <h2><u>Titel</u></h2>
+                        </div>
+                        <hr>
+                        <div class=artikel>
+                            <b>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Curabitur at erat at tortor imperdiet maximus 
+                            consequat et sem. Fusce finibus dolor non augue 
+                            ullamcorper, non efficitur massa iaculis. Pellentesque 
+                            commodo vulputate nisi, id pellentesque diam 
+                            vehicula a. Maecenas libero tortor, consequat vel 
+                            bibendum eu, rutrum eget nisl. Integer gravida, elit eu 
+                            tempor blandit, enim ante vestibulum lectus, id 
+                            pharetra dui sapien ac enim. Sed non ultricies ipsum, 
+                            at porta mi. Pellentesque sed tincidunt massa. Mauris 
+                            luctus urna metus, in faucibus ex euismod eget. Sed 
+                            tristique enim euismod, aliquam nunc eu, rutrum felis. 
+                            Vestibulum eget efficitur purus. Nullam sollicitudin, 
+                            odio nec tincidunt consequat, urna odio imperdiet 
+                            odio, non consectetur turpis mi sed enim. Aliquam 
+                            efficitur id urna nec bibendum. Nam sollicitudin, nulla 
+                            et accumsan blandit, lectus urna porttitor felis, sed 
+                            tempor nisl sapien ut tellus. Sed pellentesque lectus
+                            Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Curabitur at erat at tortor imperdiet maximus 
+                            consequat et sem. Fusce finibus dolor non augue 
+                            ullamcorper, non efficitur massa iaculis. Pellentesque 
+                            commodo vulputate nisi, id pellentesque diam 
+                            vehicula a. Maecenas libero tortor, consequat vel 
+                            bibendum eu, rutrum eget nisl. Integer gravida, elit eu 
+                            tempor blandit, enim ante vestibulum lectus, id 
+                            pharetra dui sapien ac enim. Sed non ultricies ipsum, 
+                            at porta mi. Pellentesque sed tincidunt massa. Mauris 
+                            luctus urna metus, in faucibus ex euismod eget. Sed 
+                            tristique enim euismod, aliquam nunc eu, rutrum felis. 
+                            Vestibulum eget efficitur purus. Nullam sollicitudin, 
+                            odio nec tincidunt consequat, urna odio imperdiet 
+                            odio, non consectetur turpis mi sed enim. Aliquam 
+                            efficitur id urna nec bibendum. Nam sollicitudin, nulla 
+                            et accumsan blandit, lectus urna porttitor felis, sed 
+                            tempor nisl sapien ut tellus. Sed pellentesque lectus 
+                            </b>
+                        </div>
+                    </div>
+                </div>
             </main>
         </div>
         <footer>

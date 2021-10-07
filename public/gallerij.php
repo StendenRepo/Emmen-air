a079bce4bf4c38aa71b7f2bbe8ac6f95a5cd6ec3
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2021-10-07 10:56:54 +0200

    front-end van de gallerij posting pagina zo goed als gemaakt, nog niet interactief.

---
+++ public/gallerij.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>gallerij</title>
+</head>
+<body>
+  <div id="container">
+    <header>
+      <div class="logo">
+        <h1>Emmen Air</h1>
+      </div>
+      <nav class="nav-desktop">
+        <a href="#">Home</a>
+        <a href="#">Berichten</a>
+        <a href="#">Vliegroutes</a>
+        <a href="#">Galerij</a>
+        <a href="#">Uitloggen</a> 
+      </nav>
+    </header>
+    <main>
+      <div id="main-body">
+        <div id="main-column">
+          <div class="upload">
+            <div>
+              <button type="button">Upload een afbeelding</button>
+            </div>
+          </div>
+          <div class="bericht">
+            <div>
+              <p>Bericht</p>
+            </div>
+          </div>
+          <div class="upload">
+            <div>
+              <button type="button">Plaats afbeelding</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </main>
+  </div>
+  
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
   <?php include "../templates/header.php"; ?>
+  
 </head>
 <body>
-
+    <p><a href="gallerij.php">gallerij</a></p>   
 </body>
 </html>

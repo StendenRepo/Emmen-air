cf458c003c6a2abf5819332f51cd3f5ef7c514fe
VictorP03 <83514613+VictorP03@users.noreply.github.com>
2021-10-10 16:06:54 +0200

    header verwijderd

--- public/infopage.php
+++ public/infopage.php
@@ -4,28 +4,6 @@
         <?php include "../templates/header.php"; ?>
     </head>
     <body>
-        <header>
-            <div class="logo">
-                <h1>Emmen Air</h1>
-            </div>
-            <div class="openMenu">
-                <i class="fa fa-bars"></i>
-            </div>
-            <ul class="nav-desktop">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Berichten</a></li>
-                <li><a href="#">Vliegroutes</a></li>
-                <li><a href="#">Galerij</a></li>
-                <li><a href="#">Uitloggen</a>
-            </ul>
-            <ul class="nav-mobile">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Berichten</a></li>
-                <li><a href="#">Vliegroutes</a></li>
-                <li><a href="#">Galerij</a></li>
-                <li><a href="#">Uitloggen</a></li>
-            </ul>
-        </header>
         <div id="container">
             <main class="infopagina">
                 <div class="artikel-box">

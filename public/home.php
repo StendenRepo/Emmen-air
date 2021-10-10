16a63795978526c5188dca98d114374e9e310920
VictorP03 <83514613+VictorP03@users.noreply.github.com>
2021-10-10 15:55:48 +0200

    header verwijderd

--- public/home.php
+++ public/home.php
@@ -9,28 +9,6 @@
 </head>
 <body>
     <div class="overlay"></div>
-        <header>
-            <div class="logo">
-                <h1>Emmen Air</h1>
-            </div>
-            <div class="openMenu">
-                <i class="fa fa-bars"></i>
-            </div>
-            <ul class="nav-desktop">
-                <li><a href="#">Home</a></li>
-                <li><a href="berichten.php">Berichten</a></li>
-                <li><a href="#">Vliegroutes</a></li>
-                <li><a href="#">Galerij</a></li>
-                <li><a href="#">Uitloggen</a>
-            </ul>
-            <ul class="nav-mobile">
-                <li><a href="#">Home</a></li>
-                <li><a href="berichten.php">Berichten</a></li>
-                <li><a href="#">Vliegroutes</a></li>
-                <li><a href="#">Galerij</a></li>
-                <li><a href="#">Uitloggen</a></li>
-            </ul>
-        </header>
     <div id="container">
         <main class="home">
             <div class="info">

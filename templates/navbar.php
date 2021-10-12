5ad3f31906c6e05375f83afb7097df96cf2aa9eb
VictorP03 <83514613+VictorP03@users.noreply.github.com>
2021-10-12 11:22:58 +0200

    navbar aangepast

--- templates/navbar.php
+++ templates/navbar.php
@@ -5,12 +5,12 @@
     <i class="fa fa-bars"></i>
 </div>
 <ul class="nav-desktop">
-    <li><a href="#">Home</a></li>
-    <li><a href="?language=eng">EN</a>/<a href="?language=nl">NL</a></li>
-    <li><a href="berichten.php">Berichten</a></li>
-    <li><a href="#">Vliegroutes</a></li>
-    <li><a href="#">Galerij</a></li>
-    <li><a href="#">Uitloggen</a>
+    <p><li><a href="#">Home</a></li></p>
+    <p><li><a href="?language=eng">EN</a>/<a href="?language=nl">NL</a></li></p>
+    <p><li><a href="berichten.php">Berichten</a></li></p>
+    <p><li><a href="#">Vliegroutes</a></li></p>
+    <p><li><a href="#">Galerij</a></li></p>
+    <p><li><a href="#">Uitloggen</a></p>
 </ul>
 <ul class="nav-mobile">
     <li><a href="#">Home</a></li>

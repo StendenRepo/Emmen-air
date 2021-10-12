ee6b693dc9af8f719028433b033ec8f07c0d56c8
Akash Skelly <akash.sarat@outlook.com>
2021-10-12 12:08:57 +0200

    `gallerij

---
+++ public/gallerij.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "../templates/header.php" ?>
+</head>
+<body>
+<div id="containergallerij">
+    <main class="home">
+        <div class="witvak">
+            <div class="witvak-header"></div>
+                <div class="witvak-element">
+                    <div class="image">
+                        <img src="./assets/F-16_June_2008.jpg" alt="plaatje">
+                    </div>
+                </div>
+        </div>
+        <div class="witvak">
+            <div class="witvak-header"></div>
+            <div class="witvak-element">
+                <div class="image">
+                    <img src="./assets/F-16_June_2008.jpg" alt="plaatje">
+                </div>
+            </div>
+        </div>
+        <div class="uploadinfo">
+            <div class="number">
+                <div class="number-search">
+                    <h2>Upload</h2>
+                </div>
+                <div class="number-header">
+                    <h2>Upload ook jouw eigen foto's</h2>
+                </div>
+            </div>
+        </div>
+        <div class="witvak">
+            <div class="witvak-header">
+            </div>
+                <div class="witvak-element">
+                    <div class="image">
+                        <img src="./assets/F-16_June_2008.jpg" alt="plaatje">
+                    </div>
+                </div>
+        </div>
+        <div class="witvak">
+            <div class="witvak-header"></div>
+            <div class="witvak-element">
+                <div class="image">
+                    <img src="./assets/F-16_June_2008.jpg" alt="plaatje">
+                </div>
+            </div>
+        </div>
+        <div class="witvak">
+            <div class="witvak-header"></div>
+            <div class="witvak-element">
+                <div class="image">
+                    <img src="./assets/F-16_June_2008.jpg" alt="plaatje">
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+</body>
+</html>

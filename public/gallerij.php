b74a44f536ed59e628fcdfc862b4f2031cc59bca
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2021-10-13 15:06:36 +0200

    label elements in de form geplaatst

--- public/gallerij.php
+++ public/gallerij.php
@@ -10,25 +10,25 @@
 <body>
     <main class="main-gallerij">
       <div id="main-body">
-        <div id="main-column">
-          <div class="upload"> 
-            <label for="upload-file" class="input-form"> Upload afbeelding </label>                            
-          </div>
+        <div id="main-column">       
           <form class="input-form" action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="upload"> 
+              <label for="upload-file" class="label-form"> Upload afbeelding </label>                            
+            </div>
             <input id="upload-file" type="file" name="file">  
             <button id="submit-image" type="submit" name="submit"></button> 
             <div class="message">     
-              <div class="English">
+              <div class="english">
                 <textarea name="messageEN" class="message-area" placeholder="EN"></textarea> 
               </div>   
-              <div class="Dutch">
+              <div class="dutch">
                 <textarea name="messageNL" class="message-area" placeholder="NL"></textarea> 
               </div>          
+            </div>                 
+            <div class="upload" >       
+              <label for="submit-image" class="label-form"> Plaats Afbeelding </label>                
             </div>
-          </form>          
-          <div class="upload" >       
-              <label for="submit-image" class="input-form"> Plaats Afbeelding </label>                
-          </div>
+          </form>
         </div>
       </div>     
     </main>

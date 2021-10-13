f582ada1aede3ec07d89afe74423fd1271d1785a
Tamme <wintam@live.nl>
2021-10-13 12:49:03 +0200

    enctype toegevoegd aan de form

--- public/formberichten.php
+++ public/formberichten.php
@@ -10,7 +10,7 @@ Opdracht: Project, berichten pagina.
     <?php include "../templates/header.php"; ?>
 </head>
 <body>
-  <form action="berichten.php" method="post">
+  <form action="berichten.php" method="post" enctype="multipart/form-data">
     <div id="container-berichten">
     <div class="titel-bericht">
         <input type="text" id="titelAfbeelding" name="titel_berichten" autocomplete="off" placeholder="Titel">

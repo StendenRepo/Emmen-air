d60125f9fbd031d75a9e8853b5a3517457ed30d9
JesseOost <jesse.oost@student.nhlstenden.com>
2021-10-13 10:55:47 +0200

    pagina voor vliegroutes met 2 voorbeeld vliegroute

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <?php include "../templates/header.php"; ?>
-</head>
-<body>
-
-</body>
+  <head>
+    <?php // Nog niet aanwezig include "../templates/header.php"; ?>
+  </head>
+  <body>
+    <a href="vliegroutes.php">vliegroutes</a>
+  </body>
 </html>
---
+++ public/vliegroutes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <?php // Nog niet aanwezig include "../templates/header.php"; ?>
+    <meta charset="UTF-8">
+    <title>Emmen Air - Vliegroutes</title>
+    <link href="stylesheet.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+    <!-- Tijdelijke header voor opvulling -->
+    <div id="temp-header"></div>
+    
+    <div id="flightcontainer">
+      <div class="flightroutes">
+        <img src="flightroutes/ams-dub.png" alt="Amsterdam naar Dubai" class="routeimage">
+        <hr>
+      </div>
+
+      <div class="flightroutes">
+        <img src="flightroutes/laagvliegroutes.jpg" alt="Lage vliegrotues" class="routeimage">
+        <hr>
+      </div>
+    </div>
+  </body>
+</html>

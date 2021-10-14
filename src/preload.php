f950446ea1e74871a8545710a2b11c99f82fcfb5
Cedric Smit <cedsmit@gmail.com>
2021-10-14 22:44:05 +0200

    preload
    usefull for functions that need to be called before any html is sent.

--- src/preload.php
+++ src/preload.php
@@ -1,6 +1,8 @@
 <?php
 // Check if there is no sessions. if there is no session, start a new one.
-if(session_status() === PHP_SESSION_NONE) session_start();
+if(session_status() !== PHP_SESSION_ACTIVE) {
 
+  session_start();
+}
 
 ?>

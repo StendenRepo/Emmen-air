34ad6cd64dafa01c6333582614990dfc00c73c41
Cedric Smit <cedsmit@gmail.com>
2021-10-24 19:52:28 +0200

    Block non logged in user from vieweing the page

--- src/preload.php
+++ src/preload.php
@@ -5,4 +5,16 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
   session_start();
 }
 
+/**
+ * Check if the user is logged in. if not return them to the login page.
+ */
+if (strpos($_SERVER['SCRIPT_NAME'], 'index.php') === false && !isset($_SESSION["userEmail"])) {
+  /**
+   * Please Login Message
+   */
+  $_SESSION["error"] = "you have to be logged in.";
+  header("Location: ../index.php");
+  exit;
+}
+
 ?>

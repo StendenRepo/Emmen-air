fe6c27d809f802d224ebd9749b2854d599517ee1
Cedric Smit <cedsmit@gmail.com>
2021-10-26 17:50:20 +0200

    Finished adding of message to the json file

--- public/user/uploadMessage.php
+++ public/user/uploadMessage.php
@@ -1,19 +1,17 @@
 <?php
 
-if(session_status() !== PHP_SESSION_ACTIVE) session_start();
-
+require("../../src/preload.php");
 require "../../src/translate.php";
 require "../../utils/formValidation.php";
 require "../../src/messages.php";
 
 if (isset($_POST["submit_berichten"])) {
   $title = $_POST["titel_berichten"];
-  $file = $_FILES["file"];
   $contentNl = $_POST["messageNL"];
   $contentEn = $_POST["messageEN"];
   $userId = $_SESSION["userEmail"];
 
-  if(empty($title) || $file["error"] > UPLOAD_ERR_OK || empty($contentNl) || empty($contentEn)){
+  if(empty($title) || empty($contentNl) || empty($contentEn)){
     /**
      * form was not correctly filled in return to the form page with errors.
      */
@@ -21,16 +19,8 @@ if (isset($_POST["submit_berichten"])) {
     exit;
   }
 
-  $validatedFile = validateImageAndMove($file);
-  if($validatedFile === false){
-    echo "file was not validated correctly";
-    exit;
-  }
-
-  var_dump($userId);
-  setMessage($userId, $title, $contentNl, $contentEn, $file);
-
-  echo "form was validated and moved";
+  setMessage($userId, $title, $contentNl, $contentEn);
+  header("Location:// ../home.php");
   exit;
 }
 
--- src/messages.php
+++ src/messages.php
@@ -1,15 +1,26 @@
 <?php
 
-function getAllMessages() {
+function getAllMessages($limit = null) {
 
+
+
+  return getParsedData();
 }
 
-function getMessage() {
+function setMessage($userId, $title ,$contentNl, $contentEn) {
+  $data = getParsedData();
 
+  $tmpData = array("id" => $userId, "title" => $title, "content_nl" => $contentNl, "content_en" => $contentEn);
+  array_unshift($data, $tmpData);
+  $jsonParsedData = json_encode($data);
+
+  file_put_contents(dirname(__FILE__)."/../data/post.json", $jsonParsedData);
+
+  return true;
 }
 
-function setMessage($userId, $title ,$contentNl, $contentEn, $file) {
-
+function getParsedData() {
+  return json_decode(file_get_contents(dirname(__FILE__)."/../data/post.json"), true);
 }
 
 ?>

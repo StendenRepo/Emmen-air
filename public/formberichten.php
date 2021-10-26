db2cc567f355505311fee7dd04d650b3edc14609
Michel - gaming laptop <michel-disbergen@hotmail.com>
2021-10-26 14:06:27 +0200

    updated navbar so that logo stays the same on all scales, updated padding on formberichten.php because it was giving a weird visual together with the navbar

--- public/formberichten.php
+++ public/formberichten.php
@@ -9,31 +9,32 @@ Opdracht: Project, berichten pagina.
 <head>
   <?php include "../templates/header.php"; ?>
 </head>
-<body>
-  <?php include '../templates/navbar.php'; ?>
-  <form action="berichten.php" method="post" enctype="multipart/form-data">
-    <div id="container-berichten">
-      <div class="titel-bericht">
-        <input type="text" id="titelAfbeelding" name="titel_berichten" autocomplete="off" placeholder="Titel">
-      </div>
-      <div class="item-1">
-        <label class="uploadImage" for="fotoFile">Upload een afbeelding</label>
-        <input id="fotoFile" name="foto_berichten" type="file">
-      </div>
-      <div class="item-2">
-        <textarea class="textareaBerichtenNL" name="textarea_nl" autocomplete="off"
-          placeholder="Vul hier uw bericht in..." required></textarea>
-      </div>
-      <div class="item-2-en">
-        <textarea class="textareaBerichtenEN" name="textarea_EN" autocomplete="off" placeholder="Type your message..."
-          required></textarea>
-      </div>
-      <div class="item-3">
-        <label class="submitImageButton" for="submitImage">Plaats Bericht</label>
-        <input id="submitImage" name="submit_berichten" type="submit">
-      </div>
-    </div>
-  </form>
-  <?php include "../templates/footer.php"; ?>
-</body>
+  <body>
+    <?php include '../templates/navbar.php'; ?>
+    <main>
+      <form action="berichten.php" method="post" enctype="multipart/form-data">
+        <div id="container-berichten">
+          <div class="titel-bericht">
+            <input type="text" id="titelAfbeelding" name="titel_berichten" autocomplete="off" placeholder="Titel">
+          </div>
+          <div class="item-1">
+            <label class="uploadImage" for="fotoFile">Upload een afbeelding</label>
+            <input id="fotoFile" name="foto_berichten" type="file">
+          </div>
+          <div class="item-2">
+            <textarea class="textareaBerichtenNL" name="textarea_nl" autocomplete="off"
+              placeholder="Vul hier uw bericht in..." required></textarea>
+          </div>
+          <div class="item-2-en">
+            <textarea class="textareaBerichtenEN" name="textarea_EN" autocomplete="off" placeholder="Type your message..."
+              required></textarea>
+          </div>
+          <div class="item-3">
+            <label class="submitImageButton" for="submitImage">Plaats Bericht</label>
+            <input id="submitImage" name="submit_berichten" type="submit">
+          </div>
+        </div>
+      </form>
+    </main>
+  </body>
 </html>

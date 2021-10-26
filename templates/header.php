b4623b4c5291ad7655eecf4fea393f186bb3e6cf
NHL-Michel <89914058+NHL-Michel@users.noreply.github.com>
2021-10-26 14:07:57 +0200

    updated header.php - removed title

--- templates/header.php
+++ templates/header.php
@@ -3,4 +3,3 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" href="stylesheet.css" />
 <script type="text/javascript" src="app.js"></script>
-<title>Emmen air</title>
